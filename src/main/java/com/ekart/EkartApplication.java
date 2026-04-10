5726c07aa718034761e8c1e53ffae6c331e8c6e2
Aman Shukla <amanshukla167@gmail.com>
2026-04-11 01:51:36 +0530

    added

--- src/main/java/com/ekart/EkartApplication.java
+++ src/main/java/com/ekart/EkartApplication.java
@@ -9,7 +9,7 @@ public class EkartApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EkartApplication.class, args);
 		
-		System.out.println("from the master branch");
+		System.out.println("from the master(rename it)branch");
 		System.out.println("added the commment");
 		System.out.println("rennamed the master branch to the aman");
 		System.out.println("adedd from the newBranch branch");
