97e610f2ce596ad67480efeace5d7c84326889e9
Aman Shukla <amanshukla167@gmail.com>
2026-04-13 11:50:24 +0530

    removed the syso Statement.

--- src/main/java/com/ekart/EkartApplication.java
+++ src/main/java/com/ekart/EkartApplication.java
@@ -9,10 +9,6 @@ public class EkartApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EkartApplication.class, args);
 		
-		System.out.println("from the aman(create a confilt by renamig it) branch");
-		System.out.println("added the commment");
-		System.out.println("rennamed the master branch to the aman");
-		System.out.println("adedd from the newBranch branch");
 	}
 
 }
