b670fc238c2c8ae6f490eb11cd069d4724678011
Aman Shukla <amanshukla167@gmail.com>
2026-04-14 18:52:48 +0530

    added a cart product Entity class

--- src/main/java/com/ekart/entity/Card.java
+++ src/main/java/com/ekart/entity/Card.java
@@ -1,5 +1,72 @@
 package com.ekart.entity;
 
-public class Card {
+import java.time.LocalDate;
 
+public class Card {
+	
+	private Integer cardID;
+	private String cardType;
+	private String cardNumber;
+	private String cvv;
+	private LocalDate expiryDate;
+	private String nameOnCard;
+	private String customerEmailId;
+	
+	public Integer getCardID() {
+		return cardID;
+	}
+	
+	public void setCardID(Integer cardID) {
+		this.cardID = cardID;
+	}
+	
+	public String getCardType() {
+		return cardType;
+	}
+	public void setCardType(String cardType) {
+		this.cardType = cardType;
+	}
+	public String getCardNumber() {
+		return cardNumber;
+	}
+	
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	
+	public String getCvv() {
+		return cvv;
+	}
+	
+	public void setCvv(String cvv) {
+		this.cvv = cvv;
+	}
+	
+	public LocalDate getExpiryDate() {
+		return expiryDate;
+	}
+	
+	public void setExpiryDate(LocalDate expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+	
+	public String getNameOnCard() {
+		return nameOnCard;
+	}
+	
+	public void setNameOnCard(String nameOnCard) {
+		this.nameOnCard = nameOnCard;
+	}
+	
+	public String getCustomerEmailId() {
+		return customerEmailId;
+	}
+	
+	public void setCustomerEmailId(String customerEmailId) {
+		this.customerEmailId = customerEmailId;
+	}
+	
+	
+	
+    
 }
