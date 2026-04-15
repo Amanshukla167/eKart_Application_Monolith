8c0f1d87aea2796ff856213a97bdec66a2cf00ee
Aman Shukla <amanshukla167@gmail.com>
2026-04-15 20:55:04 +0530

    added two entity file

---
+++ src/main/java/com/ekart/entity/Order.java
@@ -0,0 +1,20 @@
+package com.ekart.entity;
+
+import java.time.LocalDateTime;
+
+public class Order {
+
+	
+	
+	private Integer orderId;
+	
+	private LocalDateTime dateOfOrder;
+	
+	private Double discount;
+	
+	private Double totalPrise;
+	
+	
+	
+	
+}
