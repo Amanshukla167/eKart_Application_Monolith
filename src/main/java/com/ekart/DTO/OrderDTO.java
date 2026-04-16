bfaa4cbd247464210dc9f4552a65b0365412c19a
Aman Shukla <amanshukla167@gmail.com>
2026-04-16 20:36:37 +0530

    adde new changes

--- src/main/java/com/ekart/DTO/OrderDTO.java
+++ src/main/java/com/ekart/DTO/OrderDTO.java
@@ -24,5 +24,88 @@ public class OrderDTO {
 	private String deliveryAddress;
 	
 	private List<OrderedProductDTO> orderedProducts;
+
+	public Integer getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getCustomerEmailId() {
+		return customerEmailId;
+	}
+
+	public void setCustomerEmailId(String customerEmailId) {
+		this.customerEmailId = customerEmailId;
+	}
+
+	public LocalDateTime getDateOfOrder() {
+		return dateOfOrder;
+	}
+
+	public void setDateOfOrder(LocalDateTime dateOfOrder) {
+		this.dateOfOrder = dateOfOrder;
+	}
+
+	public Double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(Double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+
+	public Double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(Double discount) {
+		this.discount = discount;
+	}
+
+	public String getPaymentThrough() {
+		return paymentThrough;
+	}
+
+	public void setPaymentThrough(String paymentThrough) {
+		this.paymentThrough = paymentThrough;
+	}
+
+	public LocalDateTime getDateOfDelivery() {
+		return dateOfDelivery;
+	}
+
+	public void setDateOfDelivery(LocalDateTime dateOfDelivery) {
+		this.dateOfDelivery = dateOfDelivery;
+	}
+
+	public String getDeliveryAddress() {
+		return deliveryAddress;
+	}
+
+	public void setDeliveryAddress(String deliveryAddress) {
+		this.deliveryAddress = deliveryAddress;
+	}
+
+	public List<OrderedProductDTO> getOrderedProducts() {
+		return orderedProducts;
+	}
+
+	public void setOrderedProducts(List<OrderedProductDTO> orderedProducts) {
+		this.orderedProducts = orderedProducts;
+	}
+	
+	
+	
 	
 }
